92b339eddb9c65fbd3ffcbba6bbbab264cf6ecde
Eike Rehwald <rehwalde@gmail.com>
2023-04-21 10:45:46 +0200

    fixed ocr errors 3d plotting now works, only need deformation now

--- direct_stiffness_3d.py
+++ direct_stiffness_3d.py
@@ -81,21 +81,23 @@ def TrussAnalysis():
   angle = d.T/L
   a = np.concatenate((-angle.T,angle.T), axis=1)
   K = np.zeros([NDOF,NDOF])
-  for k range(NE):
+  for k in range(NE):
     aux = 2*bars[k,:]
-    index = np.r_[aux[0]:aux[0]+2,aux[1]:aux[1]+2]\
+    index = np.r_[aux[0]:aux[0]+2,aux[1]:aux[1]+2]
     ES = np.dot(a[k][np.newaxis].T*E*A,a[k][np.newaxis])/L[k]
-    K[np:ix_(index,index)] = K[np.ix_(index,index)] + ES 
+    K[np.ix_(index,index)] = K[np.ix_(index,index)] + ES
   freeDOF = DOFCON.flatten().nonzero()[0]
   supportDOF = (DOFCON.flatten() == 0).nonzero()[0]
-  Kff = K[np.ix_(freeD0F,freeD0F)]
-  Kfr = K[np.ix_(freeD0F,supportD0F)]
-  Krf = Kfr.T Krr = K[np.ix_(supportD0F,supportD0F)]
-  Pf = P.flatten()[freeD0F] Uf = np.linalg.solve(Kff,Pf)
+  Kff = K[np.ix_(freeDOF,freeDOF)]
+  Kfr = K[np.ix_(freeDOF,supportDOF)]
+  Krf = Kfr.T
+  Krr = K[np.ix_(supportDOF,supportDOF)]
+  Pf = P.flatten()[freeDOF]
+  Uf = np.linalg.solve(Kff,Pf)
   U = DOFCON.astype(float).flatten()
-  U[freeD0F] = Uf
+  U[freeDOF] = Uf
   U[supportDOF] = Ur
-  U = U.reshape(NN,DOF)
+  U = U.reshape(NN, DOF)
   u = np.concatenate((U[bars[:,0]],U[bars[:,1]]),axis=1)
   N = E*A/L[:]*(a[:]+u[:]).sum(axis=1)
   R = (Krf[:]*Uf).sum(axis=1) + (Krr[:]*Ur).sum(axis=1)
@@ -103,11 +105,15 @@ def TrussAnalysis():
   return np.array(N), np.array(R), U
 
 def Plot(nodes,c,lt,lw,lg):
-  plt.gca(projection='3d')
+  plt.axes(projection='3d')
   for i in range(len(bars)):
-    xi, xf = nodes[bars[i3O],0], nodes[bars[i,1],0]
-    yi, yf = nodes[bars[i3O],1], nodes[bars[i,1],1]
-    zi, zf = nodes[bars[i3O],2], nodes[bars[i,1],2]
-    line, = plt.plot([xi, xf],[yi, yf],[zi, zf],color=c,linestyle=lt,linewidth=lw)
+    xi, xf = nodes[bars[i,0],0], nodes[bars[i,1],0]
+    yi, yf = nodes[bars[i,0],1], nodes[bars[i,1],1]
+    zi, zf = nodes[bars[i,0],2], nodes[bars[i,1],2]
+    line, = plt.plot([xi, xf], [yi, yf], [zi, zf], color=c, linestyle=lt, linewidth=lw)
   line.set_label(lg)
-  plt.legend(prop=f'size': 8})
+  plt.legend(prop={'size': 8})
+  plt.show()
+
+#Run test
+Plot(nodes, 'gray', '--', 1, 'Undeformed')
