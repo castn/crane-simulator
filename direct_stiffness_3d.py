26ea31780541650d2f5e09b4cf9938e52045b434
Eike Rehwald <rehwalde@gmail.com>
2023-04-21 12:32:49 +0200

    added var for levels of blocks and adjusted creation

--- direct_stiffness_3d.py
+++ direct_stiffness_3d.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.axes as axes
 
 #truss structure data
 E = 1e4
@@ -9,48 +10,79 @@ nodes = []
 bars = []
 
 #test data
-width = 2000
-height = 2000
-nodes.append([0, 0, 0])               #node 0
-nodes.append([width, 0, 0])           #node 1
-nodes.append([width, width, 0])       #node 2
-nodes.append([0, width, 0])           #node 3
-nodes.append([0, 0, height])          #node 4
-nodes.append([width, 0, height])      #node 5
-nodes.append([width, width, height])  #node 6
-nodes.append([0, width, height])      #node 7
+levels = 5
+block_width = 2000
+block_height = 2000
+# nodes.append([0, 0, 0])               #node 0
+# nodes.append([block_width, 0, 0])           #node 1
+# nodes.append([block_width, block_width, 0])       #node 2
+# nodes.append([0, block_width, 0])           #node 3
+# nodes.append([0, 0, block_height])          #node 4
+# nodes.append([block_width, 0, block_height])      #node 5
+# nodes.append([block_width, block_width, block_height])  #node 6
+# nodes.append([0, block_width, block_height])      #node 7
+for i in range(levels + 1):
+  nodes.append([0, 0, block_height * i])
+  nodes.append([block_width, 0, block_height * i])
+  nodes.append([block_width, block_width, block_height * i])
+  nodes.append([0, block_width, block_height * i])
 
-bars.append([0, 1])
-bars.append([1, 2])
-bars.append([2, 3])
-bars.append([3, 0])
-bars.append([0, 4])
-bars.append([1, 5])
-bars.append([2, 6])
-bars.append([3, 7])
-bars.append([0, 5])
-bars.append([5, 2])
-bars.append([2, 7])
-bars.append([7, 0])
-bars.append([4, 5])
-bars.append([5, 6])
-bars.append([6, 7])
-bars.append([7, 4])
-bars.append([0, 2])
-bars.append([4, 6])
+# #base beams
+# bars.append([0, 1])
+# bars.append([1, 2])
+# bars.append([2, 3])
+# bars.append([3, 0])
+# #vertical beams
+# bars.append([0, 4])
+# bars.append([1, 5])
+# bars.append([2, 6])
+# bars.append([3, 7])
+# #diagonal beams
+# bars.append([0, 5])
+# bars.append([5, 2])
+# bars.append([2, 7])
+# bars.append([7, 0])
+# #top beams
+# bars.append([4, 5])
+# bars.append([5, 6])
+# bars.append([6, 7])
+# bars.append([7, 4])
+# #base and top diagonals
+# bars.append([0, 2])
+# bars.append([4, 6])
+for i in range(levels + 1):
+  print(i)
+  #base beams
+  bars.append([0 + 4 * i, 1 + 4 * i])
+  bars.append([1 + 4 * i, 2 + 4 * i])
+  bars.append([2 + 4 * i, 3 + 4 * i])
+  bars.append([3 + 4 * i, 0 + 4 * i])
+  #base diag
+  bars.append([0 + 4 * i, 2 + 4 * i])
+  if (i < levels):
+    #vertical beams
+    bars.append([0 + 4 * i, 4 + 4 * i])
+    bars.append([1 + 4 * i, 5 + 4 * i])
+    bars.append([2 + 4 * i, 6 + 4 * i])
+    bars.append([3 + 4 * i, 7 + 4 * i])
+    #diagonal beams
+    bars.append([0 + 4 * i, 5 + 4 * i])
+    bars.append([5 + 4 * i, 2 + 4 * i])
+    bars.append([2 + 4 * i, 7 + 4 * i])
+    bars.append([7 + 4 * i, 0 + 4 * i])
 
 nodes = np.array(nodes).astype(float)
 bars = np.array(bars)
 
 #Applied forces
 P = np.zeros_like(nodes)
-P[0,0] = 1
-P[0,1] = -10
-P[0,2] = -10
-P[1,1] = -10
-P[1,2] = -10
-P[2,0] = 0.5
-P[5,0] = 0.6
+# P[0,0] = 1
+# P[0,1] = -10
+# P[0,2] = -10
+# P[1,1] = -10
+# P[1,2] = -10
+# P[2,0] = 0.5
+# P[5,0] = 0.6
 
 #Support Displacement
 Ur = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
@@ -92,17 +124,17 @@ def TrussAnalysis():
   U[supportDOF] = Ur
   U = U.reshape(NN, DOF)
   u = np.concatenate((U[bars[:,0]], U[bars[:,1]]), axis=1)
-  N = E*A/L[:]*(a[:]+u[:]).sum(axis=1)
-  R = (Krf[:]*Uf).sum(axis=1) + (Krr[:]*Ur).sum(axis=1)
+  N = E * A / L[:] * (a[:] + u[:]).sum(axis=1)
+  R = (Krf[:] * Uf).sum(axis=1) + (Krr[:] * Ur).sum(axis=1)
   R = R.reshape(4, DOF)
   return np.array(N), np.array(R), U
 
 def Plot(nodes, c, lt, lw, lg):
   plt.axes(projection='3d')
   for i in range(len(bars)):
-    xi, xf = nodes[bars[i,0],0], nodes[bars[i,1],0]
-    yi, yf = nodes[bars[i,0],1], nodes[bars[i,1],1]
-    zi, zf = nodes[bars[i,0],2], nodes[bars[i,1],2]
+    xi, xf = nodes[bars[i, 0], 0], nodes[bars[i, 1], 0]
+    yi, yf = nodes[bars[i, 0], 1], nodes[bars[i, 1], 1]
+    zi, zf = nodes[bars[i, 0], 2], nodes[bars[i, 1], 2]
     line, = plt.plot([xi, xf], [yi, yf], [zi, zf], color=c, linestyle=lt, linewidth=lw)
   line.set_label(lg)
   plt.legend(prop={'size': 10})
