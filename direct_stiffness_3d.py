eccce40c554865dd5e64a373f308c606a803ee7a
castn <carsten.wesp@online.de>
2023-04-21 15:58:42 +0200

    add option to select style of diagonal beams

--- direct_stiffness_3d.py
+++ direct_stiffness_3d.py
@@ -8,7 +8,7 @@ E = 1e4
 A = 0.111
 
 # Create Tower
-tower.create(5, True)
+tower.create(5, True, True, tower.Style.DIAGONAL)
 
 # Override Python arrays with Numpy arrays, nodes are of type float64
 nodes = tower.get_nodes()
@@ -82,6 +82,8 @@ def Plot(nodes, color, line_style, pen_width, label):
     """
     # Create 3d environment
     plt.axes(projection='3d')
+    plt.autoscale(True)
+    plt.figaspect(1)
     for i in range(len(bars)):
         # Create two variables at the same time.
         xi, xf = nodes[bars[i, 0], 0], nodes[bars[i, 1], 0]
--- tower.py
+++ tower.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 import numpy
 
 nodes = []
@@ -7,27 +9,61 @@ SEGMENT_WIDTH = 2000
 SEGMENT_HEIGHT = 2000
 
 
-def create(number_of_segments, is_hollow):
+class Style(Enum):
+    CROSS = 1
+    ZICKZACK = 2
+    DIAGONAL = 3
+
+
+def create(number_of_segments, has_horizontal, is_hollow, style_of_face):
     create_segments(number_of_segments)
-    create_beams(is_hollow, number_of_segments)
+    create_beams(number_of_segments, has_horizontal, is_hollow, style_of_face)
 
 
-def create_beams(is_hollow, number_of_segments):
+def create_beams(number_of_segments, has_horizontal, is_hollow, style_of_face):
     for i in range(number_of_segments + 1):
         print(f'Create segment: {i}')
-        create_horizontal_beams(i)
+        if has_horizontal:
+            create_horizontal_beams(i)
         if not is_hollow:
             bars.append([0 + 4 * i, 2 + 4 * i])
         if i < number_of_segments:
             create_vertical_beams(i)
-            create_diagonal_beams(i)
+            create_diagonal_beams(i, style_of_face)
 
 
-def create_diagonal_beams(i):
-    bars.append([0 + 4 * i, 5 + 4 * i])  # front face
-    bars.append([5 + 4 * i, 2 + 4 * i])  # right face
-    bars.append([2 + 4 * i, 7 + 4 * i])  # rear face
-    bars.append([7 + 4 * i, 0 + 4 * i])  # left face
+def create_diagonal_beams(i, style_of_face):
+    if style_of_face == Style.DIAGONAL:
+        bars.append([0 + 4 * i, 5 + 4 * i])  # front face
+        bars.append([5 + 4 * i, 2 + 4 * i])  # right face
+        bars.append([2 + 4 * i, 7 + 4 * i])  # rear face
+        bars.append([7 + 4 * i, 0 + 4 * i])  # left face
+    elif style_of_face == Style.CROSS:
+        # front face
+        bars.append([0 + 4 * i, 5 + 4 * i])
+        bars.append([4 + 4 * i, 1 + 4 * i])
+        # right face
+        bars.append([5 + 4 * i, 2 + 4 * i])
+        bars.append([1 + 4 * i, 6 + 4 * i])
+        # rear face
+        bars.append([2 + 4 * i, 7 + 4 * i])
+        bars.append([6 + 4 * i, 3 + 4 * i])
+        # left face
+        bars.append([7 + 4 * i, 0 + 4 * i])
+        bars.append([3 + 4 * i, 4 + 4 * i])
+    elif style_of_face == Style.ZICKZACK:
+        if i % 2 == 0:
+            # For even numbers create diagonal from left to right
+            bars.append([0 + 4 * i, 5 + 4 * i])  # front face
+            bars.append([5 + 4 * i, 2 + 4 * i])  # right face
+            bars.append([2 + 4 * i, 7 + 4 * i])  # rear face
+            bars.append([7 + 4 * i, 0 + 4 * i])  # left face
+        else:
+            # For odd numbers create diagonal from right to left
+            bars.append([4 + 4 * i, 1 + 4 * i])  # front face
+            bars.append([1 + 4 * i, 6 + 4 * i])  # right face
+            bars.append([6 + 4 * i, 3 + 4 * i])  # rear face
+            bars.append([3 + 4 * i, 4 + 4 * i])  # left face
 
 
 def create_vertical_beams(i):
