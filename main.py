c05212f3c3850c01318aabb8f2695427e883c702
castn <carsten.wesp@online.de>
2023-04-28 21:03:22 +0200

    move code from direct_stiffness.py to main.yp

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+"""
+Main Script
+"""
 
-# Press Alt+Umschalt+X to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import crane
+import plotter
+
+# Youngs Module
+E = 210e6  # 210GPa
+# Cross section of each beam
+A = 0.01  # 0.01m^2
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+Umschalt+B to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Create crane
+    crane.create_crane()
+    print(f'Total length: {crane.get_length() / 1000} m')
+    print(f'Total volume: {crane.get_length() / 1000 * A} m^3')
+    density = 7850
+    print(
+        f'Total mass: {crane.get_length() / 1000 * A * density} kg with a cost of {crane.get_length() / 1000 * A * density / 1000 * 1000} euros')
+    # crane.create_counter_jib()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Override Python arrays with Numpy arrays, nodes are of type float64
+    nodes, bars = crane.get_counter_jib()
+
+    # Run test with known data
+    # N, R, U = TrussAnalysis()
+    # print('Axial Forces (positive = tension, negative = compression)')
+    # print(N[np.newaxis].T)
+    # print('Reaction Forces (positive = upward, negative = downward)')
+    # print(R)
+    # print('Deformation at nodes')
+    # print(U)
+    plotter.plot(nodes, bars, 'gray', '--', 'Undeformed')
+    # scale = 1 #increase to make more evident in plot
+    # Dnodes = U * scale + nodes
+    # plot(nodes, 'red', '-', 'Deformed')
+    plotter.display()
