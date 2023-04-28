bdd98a227aa05321dcf93c8c3920c51c5309b504
castn <carsten.wesp@online.de>
2023-04-28 20:37:37 +0200

    extract truss analysis to file
    * give variables more understandable names

--- direct_stiffness_3d.py
+++ direct_stiffness_3d.py
@@ -5,11 +5,15 @@ Provides all functions to build a crane plot the crane and deform it
 import matplotlib.pyplot as plt
 import numpy as np
 
+import tower
+import jib
 import crane
 
 # Constants
-E = 210e6 # 210GPa?
-A = 0.01 # 0.01m^2?
+# Youngs Module
+E = 210e6  # 210GPa
+# Cross section of each beam
+A = 0.01  # 0.01m^2
 
 # Create crane
 crane.create_crane()
@@ -21,62 +25,6 @@ print(f'Total mass: {crane.get_length() / 1000 * A * density} kg with a cost of 
 
 # Override Python arrays with Numpy arrays, nodes are of type float64
 nodes, bars = crane.get_counter_jib()
-
-# Applied forces
-P = np.zeros_like(nodes)
-# P[0, 0] = 1
-# P[0, 1] = -10
-# P[0, 2] = -10
-# P[1, 1] = -10
-# P[1, 2] = -10
-# P[2, 0] = 0.5
-# P[5, 0] = 0.6
-
-# Support Displacement
-Ur = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
-# Condition of DOF (0 = fixed, 1 = free)
-DOFCON = np.ones_like(nodes).astype(int)
-DOFCON[0, :] = 0
-DOFCON[1, :] = 0
-DOFCON[2, :] = 0
-DOFCON[3, :] = 0
-
-
-def truss_analysis():
-    """Perform truss structural analysis"""
-    NN = len(nodes)
-    NE = len(bars)
-    DOF = 3
-    NDOF = DOF * NN
-    # structural analysis
-    d = nodes[bars[:, 1], :] - nodes[bars[:, 0], :]
-    L = np.sqrt((d ** 2).sum(axis=1))
-    angle = d.T / L
-    a = np.concatenate((-angle.T, angle.T), axis=1)
-    K = np.zeros([NDOF, NDOF])
-    for k in range(NE):
-        aux = DOF * bars[k, :]
-        index = np.r_[aux[0]:aux[0] + DOF, aux[1]:aux[1] + DOF]
-        ES = np.dot(a[k][np.newaxis].T * E * A, a[k][np.newaxis]) / L[k]
-        K[np.ix_(index, index)] = K[np.ix_(index, index)] + ES
-    freeDOF = DOFCON.flatten().nonzero()[0]
-    supportDOF = (DOFCON.flatten() == 0).nonzero()[0]
-    Kff = K[np.ix_(freeDOF, freeDOF)]
-    Kfr = K[np.ix_(freeDOF, supportDOF)]
-    Krf = Kfr.T
-    Krr = K[np.ix_(supportDOF, supportDOF)]
-    Pf = P.flatten()[freeDOF]
-    Uf = np.linalg.solve(Kff, Pf)
-    U = DOFCON.astype(float).flatten()
-    U[freeDOF] = Uf
-    U[supportDOF] = Ur
-    U = U.reshape(NN, DOF)
-    u = np.concatenate((U[bars[:, 0]], U[bars[:, 1]]), axis=1)
-    N = E * A / L[:] * (a[:] + u[:]).sum(axis=1)
-    R = (Krf[:] * Uf).sum(axis=1) + (Krr[:] * Ur).sum(axis=1)
-    R = R.reshape(4, DOF)
-    return np.array(N), np.array(R), U
 
 
 def plot(nodes, color, line_style, pen_width, label):
