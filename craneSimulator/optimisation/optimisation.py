8c83d652de1719aeab86295d82f973ee93f1fbf9
castn <carsten.wesp@online.de>
2023-06-12 12:27:22 +0200

    basic idea for optimization

---
+++ craneSimulator/optimisation/optimisation.py
@@ -0,0 +1,5 @@
+def optimize():
+    # Values to optimize cost of crane without destroying user creation
+    # segments, support type, evtl width of tower or heigt of jibs
+    # cost_tower = height / segments * support_type
+    return 0
