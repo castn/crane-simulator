fixed loads at front front load now gets applied to the correct nodes force arrows are also attached to the correct node and have scaling

craneSimulator/gui/plotting/plotter.py
@@ -44,45 +44,49 @@ class PlotterManager:
         set_default_settings(self.diff_ax_l, "Comparison base (old)")
         set_default_settings(self.diff_ax_r, "Current (new)")
 
-    def update_unoptimized_plots(self, nodes, deformed_nodes, beams, area_per_rod, axial_forces, end_crane_parts):
-        # Plot unoptimized crane
-        simple_plot(nodes, beams, "grey", "--", "Undeformed", self.unoptim_ax_l, end_crane_parts[1], end_crane_parts[2])
-        plot_deformation_with_grad(deformed_nodes, beams, '-', self.unoptim_ax_l, axial_forces, self.cmap_deformation)
+    def update_unoptimized_plots(self, nodes, deformed_nodes, beams, area_per_rod, axial_forces, end_crane_parts, loads):
+        """Plots unoptimized crane"""
+        simple_plot(nodes, beams, "grey", "--", "Undeformed", self.unoptim_ax_l,
+                    end_crane_parts[1], end_crane_parts[2], loads[0], loads[1])
+        plot_deformation_with_grad(deformed_nodes, beams, '-', self.unoptim_ax_l,
+                                   axial_forces, self.cmap_deformation)
         plot_area_with_grad(nodes, beams, '-', self.unoptim_ax_r, self.unoptim_fig, area_per_rod)
         # print(u_ax_l.get_xticklabels())
         # u_ax_l.set_xticklabels(u_ax_l.get_xticklabels(), rotation=45)
         # u_ax_l.set_zticklabels(u_ax_l.get_zticklabels(), rotation=45)
 
-    def update_optimized_plots(self, nodes, opt_deformed_nodes, beams, opt_area_per_rod, optm_axial_forces,
-                               end_crane_parts):
-        # Plot optimized crane
-        simple_plot(nodes, beams, "grey", "--", "Undeformed", self.optim_ax_l, end_crane_parts[1], end_crane_parts[2])
-        plot_deformation_with_grad(opt_deformed_nodes, beams, '-', self.optim_ax_l, optm_axial_forces,
-                                   self.cmap_deformation)
+    def update_optimized_plots(self, nodes, opt_deformed_nodes, beams, opt_area_per_rod,
+                               optm_axial_forces, end_crane_parts, loads):
+        """Plot optimized crane"""
+        simple_plot(nodes, beams, "grey", "--", "Undeformed", self.optim_ax_l,
+                    end_crane_parts[1], end_crane_parts[2], loads[0], loads[1])
+        plot_deformation_with_grad(opt_deformed_nodes, beams, '-', self.optim_ax_l,
+                                   optm_axial_forces, self.cmap_deformation)
         plot_area_with_grad(nodes, beams, '-', self.optim_ax_r, self.optim_fig, opt_area_per_rod)
         # print(u_ax_l.get_xticklabels())
         # u_ax_l.set_xticklabels(u_ax_l.get_xticklabels(), rotation=45)
         # u_ax_l.set_zticklabels(u_ax_l.get_zticklabels(), rotation=45)
 
-    def update_diff_plot(self, base_nodes, base_opt_deformed_nodes, base_beams, base_optm_axial_forces, current_nodes,
-                         current_opt_deformed_nodes, current_beams, current_optm_axial_forces, base_end_crane_parts,
-                         current_end_crane_parts):
+    def update_diff_plot(self, base_nodes, base_opt_deformed_nodes, base_beams,
+                         base_optm_axial_forces, current_nodes, current_opt_deformed_nodes,
+                         current_beams, current_optm_axial_forces, base_end_crane_parts,
+                         current_end_crane_parts, loads):
         # Comparison base
-        if not type(base_nodes) == type(None) or not type(base_opt_deformed_nodes) == type(None) or not type(
-                base_beams) == type(None) or not type(base_optm_axial_forces) == type(None):
-            simple_plot(base_nodes, base_beams, "grey", "--", "Undeformed", self.diff_ax_l, base_end_crane_parts[1],
-                        base_end_crane_parts[2])
-            plot_deformation_with_grad(base_opt_deformed_nodes, base_beams, '-', self.diff_ax_l, base_optm_axial_forces,
-                                       self.cmap_deformation)
+        if type(base_nodes) != type(None) or type(base_opt_deformed_nodes) != type(None) or type(base_beams) != type(None) or type(base_optm_axial_forces) != type(None): 
+            #not isinstance(base_nodes, None) or not isinstance(base_opt_deformed_nodes, None) or not isinstance(base_beams, None) or not isinstance(base_optm_axial_forces, None)
+            simple_plot(base_nodes, base_beams, "grey", "--", "Undeformed", self.diff_ax_l,
+                        base_end_crane_parts[1], base_end_crane_parts[2], loads[0], loads[1])
+            plot_deformation_with_grad(base_opt_deformed_nodes, base_beams, '-', self.diff_ax_l,
+                                       base_optm_axial_forces, self.cmap_deformation)
         # Current
         # TODO here we could use reuse the current instead of plotting it again saves time
         simple_plot(current_nodes, current_beams, "grey", "--", "Undeformed", self.diff_ax_r,
-                    current_end_crane_parts[1], current_end_crane_parts[2])
+                    current_end_crane_parts[1], current_end_crane_parts[2], loads[0], loads[1])
         plot_deformation_with_grad(current_opt_deformed_nodes, current_beams, '-', self.diff_ax_r,
                                    current_optm_axial_forces, self.cmap_deformation)
 
 
-def simple_plot(nodes, beams, color, line_style, label, axes, end_jib, end_cj):
+def simple_plot(nodes, beams, color, line_style, label, axes, end_jib, end_cj, front_load, back_load):
     """
         Plot nodes using matplotlib
         :param nodes: Numpy array containing the coordinates of each node in three-dimensional space
@@ -91,7 +95,7 @@ def simple_plot(nodes, beams, color, line_style, label, axes, end_jib, end_cj):
         :param line_style: Style of the edge
         :param pen_width: Width of the edge
         :param label: Name of the edge and what it should represent
-        """
+    """
 
     for i in range(len(beams)):
         # Create initial and final coordinates
@@ -108,15 +112,16 @@ def simple_plot(nodes, beams, color, line_style, label, axes, end_jib, end_cj):
         y = nodes[i, 1]
         z = nodes[i, 2]
         if i == end_jib - 1 or i == end_jib - 2:
-            axes.quiver(x, y, z, 0, 0, -180, color='r', length=5)
+            axes.quiver(x, y, z, 0, 0, -180, color='r', length=max(1, min(12.5, abs(front_load) / 20)))
         if i == end_cj - 1 or i == end_cj - 2:
-            axes.quiver(x, y, z, 0, 0, -180, color='g', length=5)
+            axes.quiver(x, y, z, 0, 0, -180, color='g', length=max(1, min(12.5, abs(back_load) / 20)))
 
     line.set_label(label)
     axes.legend(prop={'size': 10})
 
 
 def plot_deformation_with_grad(deformed_nodes, beams, line_style, ax, axial_forces, cmap_type):
+    """Plots crane with deformation of it and colors representing the axial forces of individual beams"""
     axial_forces = np.absolute(axial_forces)
     norm = mpl.colors.Normalize(min(axial_forces), max(axial_forces))
     cmap = cm.get_cmap(cmap_type.currentText())
@@ -127,17 +132,17 @@ def plot_deformation_with_grad(deformed_nodes, beams, line_style, ax, axial_forc
         dyi, dyf = deformed_nodes[beams[i, 0], 1], deformed_nodes[beams[i, 1], 1]
         dzi, dzf = deformed_nodes[beams[i, 0], 2], deformed_nodes[beams[i, 1], 2]
 
-        line = ax.plot([dxi, dxf], [dyi, dyf], [dzi, dzf], color=cmap(norm(axial_forces[i])), linestyle=line_style,
-                       linewidth=LINE_WIDTH)
+        line = ax.plot([dxi, dxf], [dyi, dyf], [dzi, dzf], color=cmap(norm(axial_forces[i])),
+                       linestyle=line_style, linewidth=LINE_WIDTH)
         value = axial_forces[i].round(decimals=0)
         value_as_norm = norm(value)
         if not value_as_norm in values_listed_in_legend:
             values_listed_in_legend[value] = value_as_norm
         line = line[0]
-    list = axial_forces
-    list.sort()
-    ax.legend(handles=create_colormap_gradient(list, cmap, "Pa"), prop={'size': 10},
-              loc='upper right', draggable=True, title='Axial forces per rod')
+    axial_forces_list = axial_forces
+    axial_forces_list.sort()
+    # ax.legend(handles=create_colormap_gradient(axial_forces_list, cmap, "Pa"), prop={'size': 10},
+            #   loc='upper right', draggable=True, title='Axial forces per rod')
 
     line.set_label("Deformed")
     ax.set_aspect("equal")
@@ -146,6 +151,7 @@ def plot_deformation_with_grad(deformed_nodes, beams, line_style, ax, axial_forc
 
 
 def plot_area_with_grad(nodes, beams, line_style, ax, fig, area_per_rod):
+    """Plots crane with colors representing the cross section area of individual beams"""
     norm = mpl.colors.Normalize(min(area_per_rod), max(area_per_rod))
     cmap = cm.get_cmap("rainbow")
     values_listed_in_legend = {}
@@ -168,6 +174,7 @@ def plot_area_with_grad(nodes, beams, line_style, ax, fig, area_per_rod):
 
 
 def create_colormap_gradient(list, cmap, suffix):
+    """Generates a gradient of a colormap"""
     cmap_legend = []
     idx = np.round(np.linspace(0, len(list) - 1, 6)).astype(int)
     for i in idx:
@@ -178,6 +185,7 @@ def create_colormap_gradient(list, cmap, suffix):
 
 
 def create_colormap_gradient_area_per_rod(dictionary, cmap, suffix):
+    """Generates a gradient of a colormap specifically for the cross section area"""
     cmap_legend = []
     for value in dictionary:
         value_as_norm = dictionary[value]

craneSimulator/simulation/analysis.py
@@ -54,7 +54,7 @@ def generate_conditions(nodes, beams):
     Conditions.area_per_beam = np.full((len(beams)), 0.0025)
 
 
-def apply_forces(window, nodes, end_tower, end_jib, end_cj_base, end_cj):
+def apply_forces(window, nodes, end_tower, end_jib, end_jib_base, end_cj_base, end_cj):
     """Applies user-entered forces"""
     Comps.nodes = nodes
     Dims.TOWER_END = end_tower
@@ -65,11 +65,13 @@ def apply_forces(window, nodes, end_tower, end_jib, end_cj_base, end_cj):
     # Applied forces
     p = np.zeros_like(nodes)
     # Force on jib
-    p[Dims.JIB_END - 2, 2] = window.jib_left_spinBox.value() * kN
-    p[Dims.JIB_END - 1, 2] = window.jib_right_spinBox.value() * kN
+    # p[Dims.JIB_END - 2, 2] = window.jib_left_spinBox.value() * kN
+    # p[Dims.JIB_END - 1, 2] = window.jib_right_spinBox.value() * kN
+    p[end_jib_base - 2, 2] += window.jib_left_spinBox.value() * kN
+    p[end_jib_base - 1, 2] += window.jib_right_spinBox.value() * kN
     # Force on counter jib
-    p[Dims.COUNTERJIB_BASE_END - 2, 2] = window.counterjib_left_spinBox.value() * kN
-    p[Dims.COUNTERJIB_BASE_END - 1, 2] = window.counterjib_left_spinBox.value() * kN
+    p[Dims.COUNTERJIB_BASE_END - 2, 2] += window.counterjib_left_spinBox.value() * kN
+    p[Dims.COUNTERJIB_BASE_END - 1, 2] += window.counterjib_left_spinBox.value() * kN
     Conditions.forces = p
 
 

craneSimulator/truss/crane.py
@@ -14,6 +14,7 @@ class Dims:
     TOWER_HEIGHT = 0
     TOWER_WIDTH = 0
     TOWER_NUM_NODES = 0
+    JIB_BASE_NUM_NODES = 0
     JIB_NUM_NODES = 0
     CJ_BASE_NUM_NODES = 0
     CJ_NUM_NODES = 0
@@ -49,6 +50,7 @@ def create_jib():
     jib.create_connected(tower.get_nodes_raw().copy(), tower.get_beams_raw().copy(),
                          Dims.TOWER_HEIGHT, Dims.TOWER_WIDTH)
     Dims.JIB_NUM_NODES = len(jib.get_nodes())
+    Dims.JIB_BASE_NUM_NODES = jib.get_end_base()
     Comps.nodes = jib.get_nodes_raw().copy()
     Comps.beams = jib.get_beams_raw().copy()
 
@@ -129,7 +131,7 @@ def set_default_dims():
 
 def get_end_parts():
     """Returns last node of the tower, jib, and counter-jib"""
-    return Dims.TOWER_NUM_NODES, Dims.JIB_NUM_NODES, Dims.CJ_BASE_NUM_NODES
+    return Dims.TOWER_NUM_NODES, Dims.JIB_BASE_NUM_NODES, Dims.CJ_BASE_NUM_NODES
 
 
 class Crane:
@@ -160,14 +162,15 @@ class Crane:
 
     def enable_gravity(self, window):
         """Applies gravity to nodes"""
-        analysis.apply_forces(window, Comps.nodes, Dims.TOWER_NUM_NODES, Dims.JIB_NUM_NODES, Dims.CJ_BASE_NUM_NODES, Dims.CJ_NUM_NODES)
+        analysis.apply_forces(window, Comps.nodes, Dims.TOWER_NUM_NODES, Dims.JIB_NUM_NODES,
+                              Dims.JIB_BASE_NUM_NODES, Dims.CJ_BASE_NUM_NODES, Dims.CJ_NUM_NODES)
         analysis.apply_gravity(np.array(Comps.nodes).astype(float), np.array(Comps.beams),
                                self.DENSITY, self.GRAVITY_CONSTANT)
 
     def reset_forces(self, window):
         """Resets forces on all nodes"""
         analysis.apply_forces(window, Comps.nodes, Dims.TOWER_NUM_NODES, Dims.JIB_NUM_NODES,
-                              Dims.CJ_BASE_NUM_NODES, Dims.CJ_NUM_NODES)
+                              Dims.JIB_BASE_NUM_NODES, Dims.CJ_BASE_NUM_NODES, Dims.CJ_NUM_NODES)
 
     def enable_wind(self, direc, force):
         """Applies wind in given direction with given force"""

craneSimulator/truss/jibs/jib.py
@@ -21,6 +21,7 @@ class Dims:
     START_HEIGHT = 0
     TOWER_WIDTH = 0
     INIT_BAR = 0
+    END_BASE = 0
 
     IS_CONNECTED = False
 
@@ -65,7 +66,8 @@ def create_connected(tower_nodes, tower_beams, tower_height, tower_width):
     Dims.TOWER_WIDTH = tower_width
     Dims.IS_CONNECTED = True
     Dims.INIT_BAR = max(np.asarray(Comps.beams).astype(
-        int).max() - 1, 0)  # wrapped in max just in case
+        int).max() - 1, 0)
+    Dims.END_BASE = len(tower_nodes)
 
     create_segments()
     create_beams()
@@ -176,10 +178,16 @@ def get_longest_beam():
     return Dims.LONGEST_BEAM
 
 
+def get_end_base():
+    """Returns last node of base of jib"""
+    return len(Comps.nodes) if Dims.SUPPORT_TYPE == 'Truss' else (len(Comps.nodes) - 1)
+
+
 def set_dims(length, height, segs, sup_type):
     """Sets dimensions of the jib to passed-through values"""
     Comps.nodes = []
     Comps.beams = []
+    Dims.END_BASE = 0
     Dims.TOTAL_LENGTH = 0
     Dims.LONGEST_BEAM = 0
 