acc366e12f0b9ef016b299ca84d554a8715c98c1
castn <carsten.wesp@online.de>
2023-08-01 21:12:09 +0200

    add jib settings view

--- src/view/LeftSide.cpp
+++ src/view/LeftSide.cpp
@@ -6,11 +6,13 @@
 #include "LeftSide.h"
 #include "src/view/settings/towersettings.h"
 #include "src/view/settings/counterjibsettings.h"
+#include "src/view/settings/jibsettings.h"
 
 LeftSide::LeftSide(QWidget *parent) : QWidget(parent) {
     leftLayout = new QVBoxLayout(this);
 
     leftLayout->addWidget(new TowerSettings(this));
+    leftLayout->addWidget(new JibSettings(this));
     leftLayout->addWidget(new CounterJibSettings(this));
 
 
---
+++ src/view/settings/jibsettings.cpp
@@ -0,0 +1,45 @@
+//
+// Created by castn on 01.08.23.
+//
+
+#include <QGridLayout>
+#include "jibsettings.h"
+#include "src/widget.h"
+
+JibSettings::JibSettings(QWidget *parent) {
+    jibSettings = new QGroupBox(this);
+    jibSettings->setTitle("Jib");
+
+    jibSettings->setLayout(createSettings());
+}
+
+QLayout *JibSettings::createSettings() {
+    auto *layout = new QGridLayout(this);
+
+    jibLength = new QSpinBox(this);
+    jibLength->setSuffix(" mm");
+    Widget::createGridRow(layout, 0, "Length", "This is a tooltip!", jibLength, this);
+
+    jibHeight = new QSpinBox(this);
+    jibHeight->setSuffix(" mm");
+    Widget::createGridRow(layout, 1, "Height", "This is a tooltip!", jibHeight, this);
+
+    jibSegments = new QSpinBox(this);
+    jibSegments->setSuffix(" mm");
+    Widget::createGridRow(layout, 2, "Segments", "This is a tooltip!", jibSegments, this);
+
+    jibSupportType = new QComboBox(this);
+    jibSupportType->addItems({"Set-back Truss", "Truss"});
+    Widget::createGridRow(layout, 3, "Support Type", "This is a tooltip!", jibSupportType, this);
+
+    heightDegrease = new QCheckBox(this);
+    heightDegrease->setText("Realistic decrease in height of support");
+    layout->addWidget(heightDegrease, 4,0);
+
+    upwardBend = new QCheckBox(this);
+    upwardBend->setText("Realistic upward bend");
+    layout->addWidget(upwardBend,5,0);
+
+
+    return layout;
+}
