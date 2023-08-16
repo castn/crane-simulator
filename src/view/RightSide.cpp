0eae741841b4b5da5e4a569613fccac4ef68aad0
erehwald <rehwalde@gmail.com>
2023-08-16 22:46:22 +0200

    improved plotting plotting should now take place using beams added file for analysis

--- src/view/RightSide.cpp
+++ src/view/RightSide.cpp
@@ -15,6 +15,8 @@
 #include <vtkCellArray.h>
 #include <vtkPolyLine.h>
 #include "RightSide.h"
+#include "src/crane/truss/components/beam.h"
+#include "src/crane/truss/components/node.h"
 
 RightSide::RightSide(QWidget *parent) : QWidget(parent) {
     renderLayout = new QVBoxLayout(this);
@@ -27,10 +29,10 @@ auto RightSide::addRenderer() -> QWidget * {
     vtkNew<vtkGenericOpenGLRenderWindow> window;
     vtkRenderWidget->setRenderWindow(window.Get());
 
-    vtkNew<vtkSphereSource> sphere;
-    sphere->SetRadius(1.0);
-    sphere->SetThetaResolution(100);
-    sphere->SetPhiResolution(100);
+    // vtkNew<vtkSphereSource> sphere;
+    // sphere->SetRadius(1.0);
+    // sphere->SetThetaResolution(100);
+    // sphere->SetPhiResolution(100);
 
     auto polyData = createBeamPlot();
 
@@ -52,27 +54,46 @@ auto RightSide::addRenderer() -> QWidget * {
 }
 
 vtkNew<vtkPolyData> RightSide::createBeamPlot() {
+    if (beams.empty()) {
+        std::cout << "Beams haven't been set yet!";
+        return;
+    }
+
     // Create five points.
-    double origin[3] = {0.0, 0.0, 0.0};
-    double p0[3] = {1.0, 0.0, 0.0};
-    double p1[3] = {0.0, 1.0, 0.0};
-    double p2[3] = {0.0, 1.0, 2.0};
-    double p3[3] = {1.0, 2.0, 3.0};
+    // double origin[3] = {0.0, 0.0, 0.0};
+    // double p0[3] = {1.0, 0.0, 0.0};
+    // double p1[3] = {0.0, 1.0, 0.0};
+    // double p2[3] = {0.0, 1.0, 2.0};
+    // double p3[3] = {1.0, 2.0, 3.0};
 
     // Create a vtkPoints object and store the points in it
     vtkNew<vtkPoints> points;
-    points->InsertNextPoint(origin);
-    points->InsertNextPoint(p0);
-    points->InsertNextPoint(p1);
-    points->InsertNextPoint(p2);
-    points->InsertNextPoint(p3);
-
     vtkNew<vtkPolyLine> polyLine;
-    polyLine->GetPointIds()->SetNumberOfIds(5);
-    for (unsigned int i = 0; i < 5; i++)
-    {
-        polyLine->GetPointIds()->SetId(i, i);
+    polyLine->GetPointIds()->SetNumberOfIds(beams.size());
+    // points->InsertNextPoint(origin);
+    // points->InsertNextPoint(p0);
+    // points->InsertNextPoint(p1);
+    // points->InsertNextPoint(p2);
+    // points->InsertNextPoint(p3);
+    // int idLoc = 0;
+
+    for (unsigned int i = 0; i < beams.size(); i++) {
+        Node startNode = beams.at(i).getStart();
+        Node endNode = beams.at(i).getEnd();
+
+        points->InsertNextPoint(startNode.getX(), startNode.getY(), startNode.getZ());
+        points->InsertNextPoint(endNode.getX(), endNode.getY(), endNode.getZ());
+
+        polyLine->GetPointIds()->SetId(2 * i, 2 * i + 1); //(idLoc, idLoc + 1)
+        // idLoc += 2;
     }
+
+    
+    // polyLine->GetPointIds()->SetNumberOfIds(5);
+    // for (unsigned int i = 0; i < 5; i++)
+    // {
+    //     polyLine->GetPointIds()->SetId(i, i);
+    // }
 
     // Create a cell array to store the lines in and add the lines to it
     vtkNew<vtkCellArray> cells;
@@ -88,4 +109,8 @@ vtkNew<vtkPolyData> RightSide::createBeamPlot() {
     polyData->SetLines(cells);
 
     return polyData;
+}
+
+void RightSide::setBeamsToRender(std::vector<Beam> beams) {
+    this->beams = beams;
 }
--- src/view/RightSide.h
+++ src/view/RightSide.h
@@ -1,7 +1,3 @@
-//
-// Created by carsten on 30.07.23.
-//
-
 #ifndef MAINWINDOW_RIGHTSIDE_H
 #define MAINWINDOW_RIGHTSIDE_H
 
@@ -10,16 +6,18 @@
 #include <QVTKOpenGLNativeWidget.h>
 #include <QVBoxLayout>
 #include <vtkPolyData.h>
+#include "src/crane/truss/components/beam.h"
 
 class RightSide : public QWidget {
 Q_OBJECT
 public:
     explicit RightSide(QWidget *parent);
     QVBoxLayout *renderLayout = nullptr;
-
+    void setBeamsToRender(std::vector<Beam> beams);
 private:
     auto addRenderer() -> QWidget *;
     vtkNew<vtkPolyData> createBeamPlot();
+    std::vector<Beam> beams;
 };
 
 
